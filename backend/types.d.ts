2c432b280290edc7001860d80d63cbb56a355b3c
thindHarminder <harminder@thind.dev>
2024-09-17 19:56:17 -0700

    FIXING TYPES

--- backend/types.d.ts
+++ backend/types.d.ts
@@ -1,9 +1,4 @@
 type Context = import("hono").Context;
 type Bindings = {
   WEBFLOW_DOMAIN: string;
-  HEALTHIE_API_KEY: string;
-  STAGING_PROVIDER_ID: string;
-  PRODUCTION_PROVIDER_ID: string;
-  OPENLOOP_STAGING_FORM_ID: string;
-  OPENLOOP_PRODUCTION_FORM_ID: string;
 };
